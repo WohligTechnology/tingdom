1bcef138ee8cfd8a2561ac1fd3e50d2f9f5ce37a
Rajmshah <raj.mshah2015@gmail.com>
2016-02-16 18:56:24 +0530

    first git

---
+++ js/app.js
@@ -0,0 +1,7 @@
+// JavaScript Document
+// var firstapp = angular.module('firstapp',[ 'phonecatControllers']);
+//
+// firstapp.config(function($httpProvider) {
+// // for http request with session
+//   $httpProvider.defaults.withCredentials = true;
+// });
---
+++ js/controller.js
@@ -0,0 +1,7 @@
+// angular.module('phonecatControllers', [])
+//
+//   .controller('headerctrl', function($scope) {
+//     $scope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams) {
+//       $(window).scrollTop(0);
+//     });
+//   });
