0e4e2df618a5cc2cfecbc44cf29500dc3bedd187
Rajmshah <raj.mshah2015@gmail.com>
2016-02-17 10:14:50 +0530

    change

--- js/app.js
+++
@@ -1,7 +0,0 @@
-// JavaScript Document
-// var firstapp = angular.module('firstapp',[ 'phonecatControllers']);
-//
-// firstapp.config(function($httpProvider) {
-// // for http request with session
-//   $httpProvider.defaults.withCredentials = true;
-// });
--- js/controller.js
+++
@@ -1,7 +0,0 @@
-// angular.module('phonecatControllers', [])
-//
-//   .controller('headerctrl', function($scope) {
-//     $scope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams) {
-//       $(window).scrollTop(0);
-//     });
-//   });
